ea17822d627dafc37535640760e7a2af6408caf4
Bangonkali <bangonkali@gmail.com>
2015-02-06 17:42:39 +0800

    Initial commit.

---
+++ textsearch/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIAutomationBlockingCoreLib;
+
+namespace textsearch
+{
+    class Automation // http://goo.gl/pMXTLv
+    {
+        CUIAutomation _automation;
+
+        public void Start()
+        {
+            _automation = new CUIAutomation();
+            EnumarateChildren(_automation.GetRootElement());
+        }
+
+        private void EnumarateChildren(IUIAutomationElement element)
+        {
+            Console.WriteLine("{0}", element.CurrentName.Trim());
+
+            
+            IUIAutomationCacheRequest cacheRequest = _automation.CreateCacheRequest();
+            cacheRequest.AddProperty(System.Windows.Automation.AutomationElement.NameProperty.Id);
+            cacheRequest.AddProperty(System.Windows.Automation.AutomationElement.ControlTypeProperty.Id);
+            cacheRequest.TreeScope = TreeScope.TreeScope_Element | TreeScope.TreeScope_Children | TreeScope.TreeScope_Subtree;
+
+            IUIAutomationCondition cond;
+            cond = _automation.CreatePropertyConditionEx(
+                System.Windows.Automation.AutomationElement.ControlTypeProperty.Id,
+                System.Windows.Automation.ControlType.Window.Id, 
+                PropertyConditionFlags.PropertyConditionFlags_IgnoreCase);
+
+            IUIAutomationElementArray elementList = element.FindAllBuildCache(TreeScope.TreeScope_Children, cond, cacheRequest);
+
+            if (elementList == null) return;
+            
+            for (int i = 0; i < elementList.Length; i++)
+            {
+                EnumarateChildren(elementList.GetElement(i));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automation automation = new Automation();
+            automation.Start();
+        }
+    }
+}
